0af1394dbe049fd90c482bb6d4797b8e2f954440
Gatomek <tomasz.galinski@gmail.com>
2026-04-05 22:59:40 +0200

    REST API response compression added

--- radar-collector/src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rabbit/RadarNotificationListener.java
+++ radar-collector/src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rabbit/RadarNotificationListener.java
@@ -6,8 +6,8 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import pl.gatomek.flightradar.radar.poc.radarcollector.application.port.in.NotificationPort;
 import pl.gatomek.flightradar.radar.poc.radarcollector.application.domain.model.AircraftNotification;
+import pl.gatomek.flightradar.radar.poc.radarcollector.application.port.in.NotificationPort;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayInputStream;
@@ -20,31 +20,37 @@ import java.util.zip.GZIPInputStream;
 @Slf4j
 @Component
 public class RadarNotificationListener {
+    private static final String GZIP = "gzip";
+    private static final String RADAR_DATA = "RADAR_DATA";
     private final ObjectMapper objectMapper;
     private final NotificationPort notificationPort;
 
-    private static final String GZIP = "gzip";
-
-    @RabbitListener(queues = "RADAR_DATA")
+    @RabbitListener(queues = RADAR_DATA)
     public void receiveMessage(Message message) {
         MessageProperties messageProperties = message.getMessageProperties();
 
-        String contentEncoding = messageProperties.getContentEncoding();
-        if (GZIP.equals(contentEncoding)) {
-            AircraftNotification an = fromGZip(message.getBody());
-            notificationPort.notify(an);
+        try {
+            String contentEncoding = messageProperties.getContentEncoding();
+            if (GZIP.equals(contentEncoding)) {
+                AircraftNotification an = fromGZip(message.getBody());
+                notificationPort.notify(an);
+                return;
+            }
+
+            throw new UnsupportedOperationException("Content encoding not supported: "
+                    + (contentEncoding != null ? contentEncoding : "none"));
+        } catch (Exception e) {
+            log.error("Receive message error", e);
         }
     }
 
-    private AircraftNotification fromGZip(byte[] bytes) {
+    private AircraftNotification fromGZip(byte[] bytes) throws IOException {
         try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)) {
             try (GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream)) {
                 try (InputStreamReader inputStreamReader = new InputStreamReader(gzipInputStream, StandardCharsets.UTF_8)) {
                     return objectMapper.readValue(inputStreamReader, AircraftNotification.class);
                 }
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to unzip content", e);
         }
     }
 }
