a008ac69b8954a031a5a7c0e01c029e6354c12fe
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-04-05 23:10:01 +0200

    radar-collector: REST API response compression added

--- radar-collector/src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/AircraftLogRestController.java
+++ radar-collector/src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/AircraftLogRestController.java
@@ -1,6 +1,7 @@
 package pl.gatomek.flightradar.radar.poc.radarcollector.adapter.in.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import pl.gatomek.flightradar.radar.poc.radarcollector.application.port.in.LogPo
 public class AircraftLogRestController {
     private final LogPort logPort;
 
-    @GetMapping("/logs")
+    @GetMapping(value = "/logs", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AircraftNotification> getAircraftNotification() {
         AircraftNotification aircraftNotification = logPort.getLogs();
         return ResponseEntity.ok( aircraftNotification);
