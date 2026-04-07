7b0f72ddddaacab20c8d13cc54a542c0d6df0c37
Gatomek <tomasz.galinski@gmail.com>
2026-04-07 11:43:57 +0200

    radar-collector: rest cross origin policy added

--- radar-collector/src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/AircraftLogRestController.java
+++ radar-collector/src/main/java/pl/gatomek/flightradar/radar/poc/radarcollector/adapter/in/rest/AircraftLogRestController.java
@@ -3,6 +3,7 @@ package pl.gatomek.flightradar.radar.poc.radarcollector.adapter.in.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.gatomek.flightradar.radar.poc.radarcollector.application.domain.model.AircraftNotification;
@@ -13,6 +14,7 @@ import pl.gatomek.flightradar.radar.poc.radarcollector.application.port.in.LogPo
 public class AircraftLogRestController {
     private final LogPort logPort;
 
+    @CrossOrigin
     @GetMapping(value = "/logs", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AircraftNotification> getAircraftNotification() {
         AircraftNotification aircraftNotification = logPort.getLogs();
